1e5694f75eeb3e3ac11c33a53928c4dada99570a
melik <melik.budak@student.vinci.be>
2023-09-25 21:25:27 +0200

    optimisation des exercice precendent

--- module1/ex1.1/app.js
+++ module1/ex1.1/app.js
@@ -5,9 +5,33 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var filmsRouter = require('./routes/films')
+var filmsRouter = require('./routes/films');
+const { count } = require('console');
 
 var app = express();
+let countGet = 0;
+let countF = 0;
+let countPost = 0;
+let countDelete = 0;
+
+app.use((req, res, next) => {
+
+    if(req.method === 'GET' && req.path === '/'){
+        countGet++;
+    }
+    if(req.method === 'GET' && req.path === '/films'){
+        countF++;
+    } 
+    if(req.method === 'POST' && req.path === '/films'){
+        countPost++;
+    } 
+    if(req.method === 'DELETE' && req.path === '/films'){
+        countDelete++;
+        console.log('zwet'+req.listenerCount('GET'))
+    }
+       console.log('request counter \n- GET / : '+countGet+'\n- GET /films : '+countF+'\n- POST /films : '+countPost+'\n- DELETE /films :'+countDelete+'\n \n \n') 
+    next();
+})
 
 app.use(logger('dev'));
 app.use(express.json());
